256bba4d84f3d22a576db847fdb6167f0c6c732e
Raunak khatun <raunakkhatun45655@gmail.com>
2025-06-15 08:36:08 +0530

    Add files via upload

---
+++ java code/age.java
@@ -0,0 +1,11 @@
+class hello{
+    public static void main(String[] args) {
+        int age=57;
+        if(age==18){
+            System.out.println("you are 18");
+        }
+        else{
+            System.out.println("you are not 18");
+        }
+    }
+}
---
+++ java code/ifExample2.java
@@ -0,0 +1,10 @@
+public class ifExample2 {
+    public static void main(String[] args) {
+        int x = 10;
+        if(x < 20){
+            System.out.println("this is if statement");
+        }
+       
+    }
+    
+}
